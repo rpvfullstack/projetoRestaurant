1345b83073206e44c6b6c4c98e3ba2a8d5e2325d
reinaldo <reinaldo.santosmg@gmail.com>
2024-06-19 20:15:49 -0300

    Atualização

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,84 +1,122 @@
-import  { useState } from "react"
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import './navbar.css'
+// <div>Navbar</div>
 
+const Navbar = () => {
+  const [menuopen, setMenuOpen] = useState(false);
 
-    // <div>Navbar</div>
+  const handleMenuItemClick = () => {
+    setMenuOpen(false);
+  };
 
-    const Navbar = ()=> {
-        const [menuopen, setMenuOpen] = useState(false);
+  const [modal, setModal] = useState(false);
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
 
-        const handleMenuItemClick = ()=> {
-            setMenuOpen(false);
-        };
+  const toogleModal = () => {
+    setModal(!modal);
+  };
 
-        const [modal, setModal] = useState(false);
-        if(modal){
-            document.body.classList.add('active-modal');
-        }else{
-            document.body.classList.remove('active-modal');
-        }
+  const handleFormSubmit = () => {};
+  return (
+    <>
+      <nav>
+        <input
+          type="button"
+          id="click"
+          checked={menuopen}
+          onChange={() => setMenuOpen}
+        />
+        <label htmlFor="click" className="menu-btn">
+          <i className="fa fa-bars"></i>
+          <div id="logo">
+            <Link to="/" className="navbar-brand">
+              <h1>Restaurant</h1>
+            </Link>
+          </div>
+        </label>
+        <ul>
+          <li>
+            <NavLink
+              activeclassname="active"
+              to="/"
+              onClick={handleMenuItemClick}
+            >
+              Inicio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" onClick={handleMenuItemClick}>
+              Sobre
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" onClick={handleMenuItemClick}>
+              Serviços
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/pricing" onClick={handleMenuItemClick}>
+              Produtos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/team" onClick={handleMenuItemClick}>
+              Equipe
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={handleMenuItemClick}>
+              Contato
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
 
-        const handleFormSubmit = ()=> {
+      <div className="">
+        <div className="">
+          {" "}
+          <div>
+            <div className=""> </div>
+            <h2 className="">Restaurant Boca Aberta</h2>
+            <p className="">Restaurant</p>
+            <h2>Assine nossa Nesletter</h2>
 
-        }
-        return(
-            <>
-            <nav>
-                <input type="button" id="click" checked={menuopen} onChange={()=> setMenuOpen}/>
-                <label htmlFor="click" className="menu-btn">
-                   <i className="fa fa-bars"></i>
-                   <div id="logo">     
-                    <Link to='/' className='navbar-brand'><h1>Restaurant</h1></Link>
-                   </div>
-                </label>
-                <ul>
-                    <li><NavLink activeclassname='active' to='/' onClick={handleMenuItemClick}>Inicio</NavLink></li>
-                    <li><NavLink to='/about' onClick={handleMenuItemClick}>Sobre</NavLink></li>
-                    <li><NavLink to='/services' onClick={handleMenuItemClick}>Serviços</NavLink></li>
-                    <li><NavLink to='/pricing' onClick={handleMenuItemClick}>Produtos</NavLink></li>
-                    <li><NavLink to='/team' onClick={handleMenuItemClick}>Equipe</NavLink></li>
-                    <li><NavLink to='/contact' onClick={handleMenuItemClick}>Contato</NavLink></li>
-                </ul>
-            </nav>
+            <p>
+              Assinatura Gratuita, assine e ganhe 20% OFF no primeiro consumo
+            </p>
 
-
-            <div className="">
-                <div className="">    <div>
-                    <div className=""> </div>
-                    <h2 className="">Restaurant Boca Aberta</h2>
-                    <p className="">Restaurant</p>
-                    <h2>Assine nossa Nesletter</h2>
-
-                    <p>Assinatura Gratuita, assine e ganhe 20% OFF no primeiro consumo</p>
-
-                    <form onSubmit={handleFormSubmit}>
-                        <input 
-                        className="" 
-                        type="email" 
-                        required 
-                        placeholder="Seu Email" 
-                        />
-                        
-                        <label htmlFor=""></label>
-
-                    </form>
-
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-
-        
-
-        </>
-        );
-    }
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className=""
+                type="email"
+                required
+                placeholder="Seu Email"
+              />
+              <span>
+                <p>
+                  <input type="checkbox" id="same" required className="in" />
+                  <label htmlFor="same">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Officia, alias eum? Impedit laudantium voluptates deserunt
+                    reprehenderit, non quam harum illo.
+                  </label>
+                </p>
+              </span>
+              <button className="btn-pr" type="submit">
+                Assine{" "}
+              </button>
+            </form>
+            <button className="close-modal" onClick={toogleModal}></button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
-
-  
-
